136f6cca438c8b97d5dd5d701f30a2f58ad0f6bf
Clayton Kruse <claytonkruse08@gmail.com>
2025-04-22 19:40:05 -0500

    implement Player.getCurrentRow()

--- src/hw4/player/Player.java
+++ src/hw4/player/Player.java
@@ -1,63 +1,71 @@
 package hw4.player;
 
 import hw4.maze.Grid;
+import hw4.maze.Row;
 
 public class Player {
-    //params
-    private Grid grid;
-    private int rowIndex;
-    private int colIndex;
+	// params
+	private Grid grid;
+	private int rowIndex;
+	private int colIndex;
 
-    //constructor
-    public Player(Grid grid, int rowIndex, int colIndex){
-        this.grid = grid;
-        this.rowIndex = rowIndex;
-        this.colIndex = colIndex;
-    }
+	// constructor
+	public Player(Grid grid, int rowIndex, int colIndex) {
+		this.grid = grid;
+		this.rowIndex = rowIndex;
+		this.colIndex = colIndex;
+	}
 
-    //move method
-    public boolean move(Movement m){
-        //get current row and col indicies
-        int changeInRow = rowIndex;
-        int changeInCol = colIndex;
+	/**
+	 * Gets the current Row.
+	 * 
+	 * @return Returns the Row.
+	 */
+	public Row getCurrentRow() {
+		return grid.getRows().get(rowIndex);
+	}
 
-        /*
-        get a cell position and use it in switch-case to 
-        determine in adjacent cells are valid for movement (ie. not a wall)
-        */
-        //TODO
-        // Cell cellTest = ???
+	// move method
+	public boolean move(Movement m) {
+		// get current row and col indicies
+		int changeInRow = rowIndex;
+		int changeInCol = colIndex;
 
-        //use them in switch case to modify position
-        //TODO
-        switch(m){
-            case UP:
-                /*
-                  if(cellTest.getUp != CellComponents.WALL && ...){
-                    changeInRow--;
-                  }
-                */
-                //else return false;
-                break;
-            case DOWN:
-                //if(cellTest.getDown != CellComponents.WALL && ...){}
-                //else return false;
-                break;
-            case LEFT:
-                //if(cellTest.getLeft != CellComponents.WALL && ...){}
-                //else return false;
-                break;
-            case RIGHT:
-                //if(cellTest.getRight != CellComponents.WALL && ...){}
-                //else return false;
-                break;
-            default:
-                return false;
-        }
+		/*
+		 * get a cell position and use it in switch-case to determine in adjacent cells
+		 * are valid for movement (ie. not a wall)
+		 */
+		// TODO
+		// Cell cellTest = ???
 
-        //update position at once afterwards
-        rowIndex = changeInRow;
-        colIndex = changeInCol;
-        return true;
-    }
+		// use them in switch case to modify position
+		// TODO
+		switch (m) {
+			case UP:
+				/*
+				 * if(cellTest.getUp != CellComponents.WALL && ...){ changeInRow--; }
+				 */
+				// else return false;
+				break;
+			case DOWN:
+				// if(cellTest.getDown != CellComponents.WALL && ...){}
+				// else return false;
+				break;
+			case LEFT:
+				// if(cellTest.getLeft != CellComponents.WALL && ...){}
+				// else return false;
+				break;
+			case RIGHT:
+				// if(cellTest.getRight != CellComponents.WALL && ...){}
+				// else return false;
+				break;
+			default:
+				return false;
+		}
+
+		// update position at once afterwards
+		rowIndex = changeInRow;
+		colIndex = changeInCol;
+		return true;
+	}
 }
